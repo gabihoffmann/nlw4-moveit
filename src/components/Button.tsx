ac52e68d9147014fe2e07493170163dfc086a30b
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-02-23 21:54:36 -0300

    :sparkles: lidando com estado do componente

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 interface ButtonProps {
   color: string;
   children?: string;
 }
 
 export function Button(props: ButtonProps){
+  const [counter, setCounter] = useState(1);
+
+  function handleIncrement(){
+    setCounter(counter+1);
+  }
+
   return(
-    <button type="button" style={{color: props.color}}>
-      Botão {props.children}
+    <button 
+      type="button" 
+      style={{color: props.color}} 
+      onClick={handleIncrement}>
+      Botão {props.children} <strong>{counter}</strong>
     </button>
   )
 }
