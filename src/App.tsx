bc6570914a70ef9947430040ab7c0afc3f50acb2
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-02-23 21:37:22 -0300

    :sparkles: lidando com componentes e suas propriedades

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from './components/Button'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>Hello NLW#4</h1>
+       <Button color="red"/>
+       <Button color="blue"/>
+       <Button color="gree">Botão children</Button>
       </header>
     </div>
   );
